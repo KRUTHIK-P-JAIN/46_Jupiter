39ad0dd8c8a11af98bbf0d094341f142b68836db
Vinay B Kariveerappanavar <71987313+Vinaybk1799@users.noreply.github.com>
2021-07-10 19:58:42 +0530

    updating directory

--- hackathon/hackathon/user_stable.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("user_header.php");
-?>
-					
-				                                     
-                    <br><center><b>WELCOME TO COORDINATOR SECTION</b></center><br>
-<?php
-include("user_footer.php");
-?>
